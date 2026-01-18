9b8a783baf21bdc82b3d5f5aa5e3201b6c4f862f
syv <cactus30301@gmail.com>
2026-01-19 05:42:31 +0900

    docs: css scale추가

---
+++ carousel/main.js
@@ -0,0 +1,5 @@
+let pagination = document.querySelector(".gamelist.ul");
+const leftbtn = document.querySelector(".left-button");
+const rightbtn = document.querySelector(".right-button");
+
+rightbtn.addEventListener("click", () => {});
