0881f637c58b2449264ae9c3cd876f436ddbb721
syv <cactus30301@gmail.com>
2026-02-02 03:39:23 +0900

    docs: info css, js 작성

---
+++ pricing-plan/assets/scripts/component.js
@@ -0,0 +1,18 @@
+// 플랜카드 클릭하면 clicked css 적용
+
+const planCards = document.querySelectorAll(".plan-card");
+const info = document.querySelector(".selection-info");
+console.log(planCards);
+
+for (const planCard of planCards) {
+  planCard.addEventListener("click", (e) => {
+    planCards.forEach((planCard) => planCard.classList.remove("clicked"));
+
+    planCard.classList.add("clicked");
+    info.classList.add("clicked");
+
+    const planName = planCard.dataset.name;
+    const planPrice = planCard.dataset.price;
+    info.textContent = `${planName} 플랜(${planPrice})을 선택하셨습니다.`;
+  });
+}
